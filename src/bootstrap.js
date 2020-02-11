75b4c0cc317703080c078a9996bbc83e644be1b3
Christian Sheen <csheen1725@gmail.com>
2020-02-10 23:50:05 -0500

    third commit

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -3,8 +3,8 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-import reducers from "./reducers";
 import thunk from "redux-thunk";
+import reducers from "./reducers";
 
 const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
 
