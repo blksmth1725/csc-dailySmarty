429e4c545c654a8f02ea3a047887885ddcb4de00
Christian Sheen <csheen1725@gmail.com>
2020-02-16 01:38:15 -0500

    added Logo and SearchBar component to results page (component) to display different logo sizes plus search bar

--- src/components/logo.js
+++ src/components/logo.js
@@ -2,9 +2,18 @@ import React, { Component } from "react";
 
 export default class Logo extends Component {
   render() {
+    const size = {
+      height: this.props.size ? this.props.size : 105,
+      width: this.props.size ? this.props.size : 105
+    };
+
     return (
       <div className="logo-main">
-        <img alt="Qmmunity-UI-image" src="/assets/qmmunity-logo.png" />
+        <img
+          style={size}
+          alt="Qmmunity-UI-image"
+          src="/assets/qmmunity-logo.png"
+        />
       </div>
     );
   }
--- src/components/results.js
+++ src/components/results.js
@@ -1,10 +1,14 @@
 import React, { Component } from "react";
 
+import Logo from "./logo";
+import SearchBar from "./search-bar";
+
 class Results extends Component {
   render() {
     return (
       <div>
-        <h1>Results</h1>
+        <Logo size={55} />
+        <SearchBar />
       </div>
     );
   }
