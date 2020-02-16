1a1f9608a1dbf3544027b3230cfbeb360a9d16a8
Christian Sheen <csheen1725@gmail.com>
2020-02-16 10:44:30 -0500

    added props to searchbar so that it is usuable in all comopnents

--- src/components/home.js
+++ src/components/home.js
@@ -5,12 +5,17 @@ import SearchBar from "./search-bar";
 import RecentPosts from "./recent-posts";
 
 export default class Home extends Component {
+  handleSearchbarSubmit = function(query) {
+    console.log("trying to handle query", query);
+    this.props.history.push("/results");
+  };
+
   render() {
     return (
       <div className="app">
         <div className="container">
           <Logo />
-          <SearchBar />
+          <SearchBar onSubmit={query => this.handleSearchbarSubmit(query)} />
           <RecentPosts />
         </div>
       </div>
--- src/components/results.js
+++ src/components/results.js
@@ -4,11 +4,15 @@ import Logo from "./logo";
 import SearchBar from "./search-bar";
 
 class Results extends Component {
+  handleSearchbarSubmit(query) {
+    console.log(query);
+  }
+
   render() {
     return (
       <div>
         <Logo size={55} />
-        <SearchBar />
+        <SearchBar onSubmit={query => this.handleSearchbarSubmit(query)} />
       </div>
     );
   }
